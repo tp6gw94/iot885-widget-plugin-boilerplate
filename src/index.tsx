ffb1e44876b8bcd76d7ad87a09239574cf2ca058
todd <tp6gw94@gmail.com>
2022-09-19 11:14:29 +0800

    update listen event

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import { usePluginWidget } from 'iot885-plugin-core';
-import React, { useEffect } from 'react';
+import { Message, MessageType, Widget } from 'iot885-plugin-core';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import packageJson from '../package.json';
 
@@ -8,14 +8,37 @@ type Meta = {
 };
 
 export const App = () => {
-  const widget = usePluginWidget<Meta>({
-    name: packageJson.name,
-    mode: process.env.REACT_APP_MODE as any,
-  });
+  const [widget, setWidget] = useState<Widget<Meta>>();
+  const [wh, setWH] = React.useState<{
+    w: number | string;
+    h: number | string;
+  }>({ w: '100%', h: 200 });
+
+  const messageEventLister = React.useCallback((evt: MessageEvent) => {
+    const { payload, type } = evt.data as Message;
+    if (type === MessageType.UPDATE) {
+      setWidget(payload);
+    }
+    if (type === MessageType.SETUP_UI) {
+      setWH(payload);
+    }
+  }, []);
 
   useEffect(() => {
     console.log(widget);
   }, [widget]);
+
+  useEffect(() => {
+    window.addEventListener('message', messageEventLister, false);
+    parent.postMessage(
+      {
+        type: MessageType.MOUNTED,
+        payload: window.iframeId,
+        source: packageJson.name,
+      },
+      '*'
+    );
+  }, []);
 
   return <div></div>;
 };
